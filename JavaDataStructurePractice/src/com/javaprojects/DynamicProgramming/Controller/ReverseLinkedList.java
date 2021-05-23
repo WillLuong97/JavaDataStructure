28fab71ff5e360267385303ab2c3ad431232c32a
Will Luong <tluong@stedwards.edu>
2021-05-22 23:09:22 -0500

    Added java code to reverse a linked list

---
+++ JavaDataStructurePractice/src/com/javaprojects/DynamicProgramming/Controller/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package com.javaprojects.DynamicProgramming.Controller;
+
+import com.javaprojects.DynamicProgramming.Model.ListNode;
+
+public class ReverseLinkedList {
+    //give the head of a linked list, print out the reverse of the linked list
+    public ListNode reverseALinkedList(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode nextNode = curr.getNext();
+            curr.setNext(prev);
+            curr = prev;
+            prev = curr;
+        }
+        return prev.getNext();
+    }
+}
