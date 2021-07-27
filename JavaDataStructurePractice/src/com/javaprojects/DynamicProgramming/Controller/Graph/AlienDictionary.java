4cc83f22d74e9f5396a392950eef274f7d78d2d6
Will Luong <tluong@stedwards.edu>
2021-07-26 19:05:11 -0500

    Added java template to start the project

---
+++ JavaDataStructurePractice/src/com/javaprojects/DynamicProgramming/Controller/Graph/AlienDictionary.java
@@ -0,0 +1,49 @@
+package com.javaprojects.DynamicProgramming.Controller.Graph;
+
+/*
+* There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+Input:
+[
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+
+Output: "wertf"
+Input:
+[
+  "z",
+  "x"
+]
+
+Output: "zx"
+Input:
+[
+  "z",
+  "x",
+  "z"
+]
+
+Output: ""
+
+Explanation: The order is invalid, so return "".
+*
+*
+*
+* You may assume all letters are in lowercase.
+You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+If the order is invalid, return an empty string.
+There may be multiple valid order of letters, return any one of them is fine.
+*
+* 
+https://massivealgorithms.blogspot.com/2019/04/leetcode-269-alien-dictionary.html
+*
+*
+*  */
+public class AlienDictionary {
+    public String alienOrder(String[] words) {
+        throw new RuntimeException("NOT YET IMPLEMENTED!");
+    }
+}
