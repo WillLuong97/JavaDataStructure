fd2b66f74fc8bd5eafa132da59ccf20214de6c61
Will Luong <tluong@stedwards.edu>
2021-08-06 21:23:00 -0500

    Added template for the BinaryTreeDiameter problem

---
+++ JavaDataStructurePractice/src/com/javaprojects/DynamicProgramming/Controller/Tree/BinaryTreeDiameter.java
@@ -0,0 +1,10 @@
+package com.javaprojects.DynamicProgramming.Controller.Tree;
+
+import com.javaprojects.DynamicProgramming.Model.TreeNode;
+
+public class BinaryTreeDiameter {
+    public int diameterOfBinaryTree(TreeNode root) {
+        throw new RuntimeException("NOT YET IMPLMENTED!");
+    }
+
+}
