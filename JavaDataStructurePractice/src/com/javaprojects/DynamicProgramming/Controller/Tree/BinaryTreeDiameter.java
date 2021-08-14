144639e2e58279f9a69666996a102c9e9c29f7db
Will Luong <tluong@stedwards.edu>
2021-08-13 22:50:00 -0500

    Added java code to solve the tree diameter leetcode problem

--- JavaDataStructurePractice/src/com/javaprojects/DynamicProgramming/Controller/Tree/BinaryTreeDiameter.java
+++ JavaDataStructurePractice/src/com/javaprojects/DynamicProgramming/Controller/Tree/BinaryTreeDiameter.java
@@ -3,8 +3,24 @@ package com.javaprojects.DynamicProgramming.Controller.Tree;
 import com.javaprojects.DynamicProgramming.Model.TreeNode;
 
 public class BinaryTreeDiameter {
+    int result;
     public int diameterOfBinaryTree(TreeNode root) {
-        throw new RuntimeException("NOT YET IMPLMENTED!");
+        result = 0;
+        int x = height(root);
+        return result;
+    }
+    //helper method to traverse the tree recrusively
+    private int height(TreeNode root){
+        if(root == null){
+            //tree is empty or we cannot continue to go forward
+            return 0;
+        }
+
+        int leftH = height(root.left);
+        int rightH = height(root.right);
+        result = Math.max(result, (leftH + rightH));
+
+        return 1 + Math.max(leftH, rightH);
     }
 
 }
